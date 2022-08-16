95340e2185296b4b36f08e37851042b4071b2d70
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-08-16 11:56:47 +0530

    main Task Completed

--- src/controller/Client1FormController.java
+++ src/controller/Client1FormController.java
@@ -6,12 +6,47 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client1FormController {
     public AnchorPane Client1;
     public TextField txtTypeMessage;
     public Label lblLabelForName;
     public TextArea txtTextArea;
 
-    public void sendMsgOnAction(ActionEvent actionEvent) {
+    final int PORT = 9000;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String receiveMessageFromClient = "";
+
+    public void initialize(){
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost", PORT);
+
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+
+                while (!receiveMessageFromClient.equals("exit")) {
+                    receiveMessageFromClient = dataInputStream.readUTF();
+                    txtTextArea.appendText(receiveMessageFromClient + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Something went Wrong in Client 1");
+            }
+        }).start();
+    }
+    public void sendMsgOnAction(ActionEvent actionEvent) throws IOException {
+        String messageToSend = txtTypeMessage.getText().trim();
+
+        dataOutputStream.writeUTF(messageToSend);
+        dataOutputStream.flush();
+        txtTextArea.appendText("You : " + messageToSend + "\n");
+        txtTypeMessage.clear();
     }
 }
--- src/controller/Client3FormController.java
+++ src/controller/Client3FormController.java
@@ -6,12 +6,48 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client3FormController {
     public AnchorPane Client3;
     public TextField txtTypeMessage;
     public Label lblLabelForName;
     public TextArea txtTextArea;
 
-    public void sendMsgOnAction(ActionEvent actionEvent) {
+    final int PORT = 9002;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String receiveMessageFromClient = "";
+
+    public void initialize(){
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost", PORT);
+
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+
+                while (!receiveMessageFromClient.equals("exit")) {
+                    receiveMessageFromClient = dataInputStream.readUTF();
+                    txtTextArea.appendText(receiveMessageFromClient + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Something went Wrong in Client 2");
+            }
+        }).start();
+    }
+
+    public void sendMsgOnAction(ActionEvent actionEvent) throws IOException {
+        String messageToSend = txtTypeMessage.getText().trim();
+
+        dataOutputStream.writeUTF(messageToSend);
+        dataOutputStream.flush();
+        txtTextArea.appendText("You : " + messageToSend + "\n");
+        txtTypeMessage.clear();
     }
 }
