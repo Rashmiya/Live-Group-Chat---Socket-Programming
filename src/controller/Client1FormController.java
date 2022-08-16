0c1a838dbf2597b1b00d0bc46666f282b3d744b7
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-08-16 11:27:14 +0530

    Created FXML files and Controller Classes for Clients and LoginPage

---
+++ src/controller/Client1FormController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Client1FormController {
+    public AnchorPane Client1;
+    public TextField txtTypeMessage;
+    public Label lblLabelForName;
+    public TextArea txtTextArea;
+
+    public void sendMsgOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane loginForm;
+    public TextField txtUserName;
+
+    public void logInOnAction(ActionEvent actionEvent) {
+    }
+}
